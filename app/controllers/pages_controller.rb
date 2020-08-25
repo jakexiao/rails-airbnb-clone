a1106312c599fc7dee4a8314880530ac8a9e161f
barbara rebelo <barbaramrebelo51@gmail.com>
2020-08-25 19:19:52 +0100

    links between pages created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
   def home
     if params[:query]
-      @flats = Flat.where("name LIKE '%#{params[:query]}%'")
+      @flats = Flat.where("name ILIKE '%#{params[:query]}%'")
     else
       @flats = Flat.all
     end
-    
   end
 end
