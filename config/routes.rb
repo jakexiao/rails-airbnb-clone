6ce306f5fd5833bead226be7a44da90823e77891
barbara rebelo <barbaramrebelo51@gmail.com>
2020-08-27 14:35:48 +0100

    bookings-model updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :flats
   get "/dashboard", to: "flats#dashboard"
+  resources :bookings
+  get "/dashboard", to: "bookings#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
