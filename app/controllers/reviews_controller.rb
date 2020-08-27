5d49ce427e7a1e3f7acba0299b7a83f5a6c53748
Daniel Schiavoni <daniel.schia@gmail.com>
2020-08-27 14:40:25 +0100

    latest version

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+      if @review.save
+        redirect_to booking_path(@booking)
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @review = Review.find(params[:id])
+      @review.destroy
+      redirect_to booking_path(@booking)
+    end
+
+    private
+
+    def review_params
+      params.require(review).permit(:comments, :rating)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :flats
   get "/dashboard", to: "flats#dashboard"
+  resources :bookings do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
